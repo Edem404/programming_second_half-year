fee29ba41ebcf82f0053015961b610e8a221bb47
love_with <rbordulyak@gmail.com>
2023-03-13 09:09:19 +0200

    adding src dir with reworked class WritingDesk

--- src/ua/lviv/iot/algo/part1/lab1/WritingDesk.java
+++ src/ua/lviv/iot/algo/part1/lab1/WritingDesk.java
@@ -5,6 +5,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.Getter;
 import lombok.ToString;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Setter
@@ -18,50 +19,39 @@ public class WritingDesk {
     private int currentHeight;
     private int maxHeight;
     private static WritingDesk instance = new WritingDesk();
-    public static WritingDesk getInstance()
-    {
+
+    public static WritingDesk getInstance() {
         return instance;
     }
+
     // increase current height of writing desk not more than the maximum value
-    public void adjustHeight(int centimeters)
-    {
-        if (currentHeight + centimeters > maxHeight)
-        {
-            System.out.println("U can't increase the height of the writing desk above the possible limit");
-        }
-        else
-        {
+    public void adjustHeight(int centimeters) {
+        if (currentHeight + centimeters > maxHeight) {
+            return;
+        } else {
             currentHeight += centimeters;
-            System.out.println("current height of writing desk: " + currentHeight);
         }
 
     }
 
     // decrease current height of writing desk but not lower than 0
-    public void moveDawn (int centimeters)
-    {
-        if (currentHeight - centimeters < 0)
-        {
-            System.out.println("U can't decrease the height of the writing desk above the possible limit");
-        }
-        else
-        {
+    public void moveDawn(int centimeters) {
+        if (currentHeight - centimeters < 0) {
+            return;
+        } else {
             currentHeight -= centimeters;
-            System.out.println("current height of writing desk: " + currentHeight);
         }
     }
 
-    public  static void main (String[] args)
-    {
+    public static void main(String[] args) {
         WritingDesk[] arrayOfWritingDesks = new WritingDesk[4];
-        arrayOfWritingDesks[0] = new WritingDesk(3, true, 200, 120 , 150);
+        arrayOfWritingDesks[0] = new WritingDesk(3, true, 200, 120, 150);
         arrayOfWritingDesks[1] = new WritingDesk();
         arrayOfWritingDesks[2] = getInstance();
         arrayOfWritingDesks[3] = getInstance();
 
-        for (int i = 0; i < arrayOfWritingDesks.length; i++)
-        {
-            System.out.println(arrayOfWritingDesks[i].toString());
+        for (WritingDesk i : arrayOfWritingDesks) {
+            System.out.println(i.toString());
         }
     }
 }
